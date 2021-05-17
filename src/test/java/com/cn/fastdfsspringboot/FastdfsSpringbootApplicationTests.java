993033c93536dcf402584b2672803167eed4f71c
Administrator <2410266658@qq.com>
2021-05-17 23:43:01 +0800

    fastdfs集成springboot实现文件上传。docker下安装fastdfs

---
+++ src/test/java/com/cn/fastdfsspringboot/FastdfsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.fastdfsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastdfsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
